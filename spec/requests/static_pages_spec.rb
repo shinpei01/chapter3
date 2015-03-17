8f67e8fa5ca44f8977ba4a1ee266a219a9bebbe9
野口真平 <shinpei@noguchimappira-no-MacBook-Air.local>
2015-03-17 20:22:41 +0900

    第４章まで終了

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,22 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  describe "Home page" do
+describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    it "should have the right title" do
-     visit '/static_pages/home'
-     expect(page).to have_title("#{base_title} | Home")    end
+  it "should have the content 'Sample App'" do
+    visit '/static_pages/home'
+    expect(page).to have_content('Sample App')
+  end
+
+  it "should have the base title" do
+    visit '/static_pages/home'
+    expect(page).to have_title("Ruby on Rails Tutorial")
+  end
+
+  it "should not have a custom page title" do
+    visit '/static_pages/home'
+    expect(page).not_to have_title('| Home')
+  end
   end
 
   describe "Help page" do
@@ -23,7 +30,23 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-     expect(page).to have_title("#{base_title} | Help")    end
+     expect(page).to have_title("#{base_title} | Help")
+     end
+
+    it "should have the content 'Sample App'" do
+       visit '/static_pages/home'
+       expect(page).to have_content('Sample App')
+     end
+
+     it "should have the base title" do
+       visit '/static_pages/home'
+       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+     end
+
+     it "should not have a custom page title" do
+       visit '/static_pages/home'
+       expect(page).not_to have_title('| Home')
+     end
   end
 
   describe "About page" do
@@ -49,4 +72,6 @@ describe "Static pages" do
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
+
+
 end
